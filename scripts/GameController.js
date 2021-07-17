6c1e114ffe6a32da1ef0f54e81d95c9f7d0fce42
Henrique <henrique.claranhan@outlook.com>
2021-07-17 18:36:34 -0400

    update methods

--- scripts/GameController.js
+++ scripts/GameController.js
@@ -1,20 +1,18 @@
 class GameController {
-    constructor(bird){
+    constructor(bird, floor){
         this._content = document.querySelector("#content");
-        this._gameIsRunning = true;
         this._bird = bird;
+        this._floor = floor;
+
+        this._gameIsRunning = true;
         this._gameAudio = new GameAudio();
 
         this.initialize();
     }
 
     initialize(){
-        this.jumpClick = () => {
-            this.bird.jump();
-            this.gameAudio.playAudio("wing")
-        }
-        
-        this.jumpClick();
+        this.executeBirdFirstActions();
+
         this.initTouchEvents();
     }
 
@@ -33,11 +31,35 @@ class GameController {
         return this._bird;
     }
 
+    get floor(){
+        return this._floor;
+    }
+
     get gameAudio(){
         return this._gameAudio;
     }
 
+    jumpClick = () => {
+        this.bird.jump();
+        this.gameAudio.playAudio("wing");
+    }
+
+    executeBirdFirstActions(){
+        this.bird.stopFlapWave();
+        this.jumpClick();
+    }
+
     initTouchEvents(){
         this.content.addEventListener("click", this.jumpClick, false);
+    }
+
+    stopTouchEvents(){
+        this.content.removeEventListener("click", this.jumpClick, false);
+    }
+
+    stopAllGameElements(){
+        this.bird.stopFlap();
+        this.floor.stopLoop();
+        this.stopTouchEvents();
     }
 }
